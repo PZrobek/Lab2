7e827971ca88776b89f78b4a4f54d2ede3ae16f9
PZrobek <37156@studentwsei.pl>
2022-05-13 00:09:31 +0200

    Licznik kliknięć do pola tekstowego (TextField)

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
@@ -3,14 +3,14 @@ package pl.lublin.wsei.java.cwiczenia.lab2;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Klikniêcie nr " + ++clickCounter);
+        txtLicznik.setText("Klikniêcie nr " + ++clickCounter);
     }
 
 }
